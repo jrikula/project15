4e0ffeb0e91095381f5b56aad210f335f26f75d0
jrikula <78084747+jrikula@users.noreply.github.com>
2021-02-10 14:20:27 +0200

    crud fix

--- crud/create.php
+++ crud/create.php
@@ -3,17 +3,31 @@ $fname= $_POST['fname'];
 $lname= $_POST['lname'];
 $city= $_POST['city'];
 $groupid= $_POST['groupid'];
-$sql="insert into studentsinfo (fname, lname, city, groupid)
-values('$fname', '$lname', '$city', '$groupid')";
 
-if($conn->query($sql) === TRUE) {
-    echo "New record added";
-    echo "<a href='update.php' class='top'>Home </a>";
+if (empty ($fname)) {
+    echo"ERROR: FIRST NAME IS REQUIRED!";
 }
-else
-{
-    echo "ERROR: " .$sql. "<br>" . $conn->error;
-}
-$conn->close();
 
+if (empty ($lname)) {
+    echo" ERROR: LAST NAME IS REQUIRED!";
+}
+
+if (empty ($city)) {
+    echo" ERROR: CITY IS REQUIRED!";
+}
+
+else {
+    $sql="insert into studentsinfo (fname, lname, city, groupid)
+    values('$fname', '$lname', '$city', '$groupid')";
+
+    if($conn->query($sql) === TRUE) {
+        echo "New record added";
+        echo "<a href='update.php' class='top'>Home </a>";
+    }
+    else
+    {
+        echo "ERROR: " .$sql. "<br>" . $conn->error;
+    }
+    $conn->close();
+}    
 ?>
--- crud/read.php
+++ crud/read.php
@@ -21,6 +21,5 @@ else
     echo "no results";
 }
 
-
 $conn->close();
 ?>
