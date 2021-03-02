73d2bac2c0c45a1cea2e0bb254099354590581d7
TT <78346361+neninapat@users.noreply.github.com>
2021-03-02 12:26:53 +0200

    Update header.php

--- header.php
+++ header.php
@@ -5,22 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com"><link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
 
 
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #a30909";>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #a30909; font-family: 'Jost', sans-serif; font-size: 20px;">
   <a class="navbar-brand" href="#">
     <img src="icon1.png" width="60" height="60" alt="">
   </a>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#" style="color: white">Current Movies <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#" style="color: white">CURRENT MOVIES <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="#" style="color: white">Reviews <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#" style="color: white">REVIEWS <span class="sr-only">(current)</span></a>
       </li>
     </ul>
   </div>
