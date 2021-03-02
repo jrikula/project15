b51c6784255e2036af6de82d59587aa9a096c055
TT <78346361+neninapat@users.noreply.github.com>
2021-03-02 13:27:20 +0200

    Add files via upload

---
+++ currentmovies.php
@@ -0,0 +1,251 @@
+<?php include "header.php" ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
+
+    <title>Current Movies</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="header">
+          <br>
+      </div>
+      
+      <div class="row">
+        <div class="col-md-4">
+          <div class="image-wrapper">
+            <img src="images/blackwidow.jpeg" alt="">
+            <div class="movie-info">
+              <p>
+                Natasha Romanoff aka Black Widow confronts the darker parts of her<br>
+                 ledger when a dangerous <br>
+                 conspiracy with ties to her past arises.<br>
+                Pursued by a force that will stop at nothing to bring her down<br>  
+              </p>
+            </div> 
+          </div>
+          <div class="button">
+            <p>
+              <a href="#"><button type="button" class="btn btn-light">Book Tickets</button></a>
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="image-wrapper">
+            <img src="images/samurai.png" alt="">
+            <div class="movie-info">
+              <p>
+                An action packed animated comedy for all the family inspired by Mel Brooks' timeless classic, Blazing Saddles. <br>
+                Hank, a loveable dog with a head full of dreams about becoming a samurai, sets off in search of his destiny.<br>   
+              </p> 
+            </div>
+          </div>
+          <div class="button">
+            <p>
+              <a href="#"><button type="button" class="btn btn-light">Book Tickets</button></a>
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4"> 
+          <div class="image-wrapper">
+            <img src="images/ghostbusters.jpg" alt="">
+            <div class="movie-info">
+              <p>
+                When a single mother and her two children move to a new town, <br>
+                they soon discover that they have a connection to the original Ghostbusters and their grandfather's secret legacy.<br>  
+              </p>
+            </div>
+          </div> 
+          <div class="button">
+            <p>
+              <a href="#"><button type="button" class="btn btn-light">Book Tickets</button></a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="image-wrapper">
+            <img src="images/ff9.jpg" alt="">
+            <div class="movie-info">
+              <p>
+                Dominic Toretto and his crew join forces to battle the most skilled assassin and high-performance driver they've ever encountered -- his forsaken brother.<br>  
+              </p>
+            </div>
+          </div>
+          <div class="button">
+            <p>
+              <a href="#"><button type="button" class="btn btn-light">Book Tickets</button></a>
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="image-wrapper">
+            <img src="images/godzilla.jpg" alt="">
+            <div class="movie-info">
+              <p>
+                Fearsome monsters Godzilla and King Kong square off in an epic battle for the ages, <br>
+                while humanity looks to wipe out both of the creatures and take back the planet once and for all.<br>  
+              </p>
+            </div>
+          </div>
+          <div class="button">
+            <p>
+              <a href="#"><button type="button" class="btn btn-light">Book Tickets</button></a>
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="image-wrapper">
+            <img src="images/bossb.png" alt="">
+            <div class="movie-info">
+              <p>
+                Now adults, Theodore Templeton and his estranged brother take a magical formula that transforms them into babies for 48 hours.<br>
+                Together, they must now go under cover to prevent an evil genius from turning fellow toddlers intro monstrous brats.<br>
+               
+              </p>
+            </div>
+          </div>
+          <div class="button">
+            <p>
+              <a href="#"><button type="button" class="btn btn-light">Book Tickets</button></a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="image-wrapper">
+            <img src="images/notime.jpg" alt="">
+            <div class="movie-info">
+              <p>
+                Recruited to rescue a kidnapped scientist, globe-trotting spy James Bond finds himself hot on the trail of a mysterious villain,<br>
+                who's armed with a dangerous new technology.<br> 
+              </p>
+            </div>
+          </div>
+          <div class="button">
+            <p>
+              <a href="#"><button type="button" class="btn btn-light">Book Tickets</button></a>
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="image-wrapper">
+            <img src="images/wrongturn.jpg" alt="">
+            <div class="movie-info">
+              <p>
+                Despite warnings to stick to the Appalachian Trail, hikers stray off course and cross into<br>
+                land inhabited by a hidden community of mountain dwellers who use deadly means to protect their way of life.<br>  
+              </p>
+            </div>
+          </div>
+          <div class="button">
+            <p>
+              <a href="#"><button type="button" class="btn btn-light">Book Tickets</button></a>
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="image-wrapper">
+            <img src="images/conjuring.jpg" alt="">
+            <div class="movie-info">
+              <p>
+                Paranormal investigators Ed and Lorraine Warren try to uncover the truth behind a murderer's claim of demonic possession.<br>
+                
+              </p>
+            </div>
+          </div>
+          <div class="button">
+            <p>
+              <a href="#"><button type="button" class="btn btn-light">Book Tickets</button></a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="image-wrapper">
+            <img src="images/raya.jpeg" alt="">
+            <div class="movie-info">
+              <p>
+                Long ago, in the fantasy world of Kumandra, humans and dragons lived together in harmony. <br>
+                However, when sinister monsters known as the Druun threatened the land, the dragons sacrificed themselves to save humanity. <br>
+              </p>
+            </div>
+          </div>
+          <div class="button">
+            <p>
+              <a href="#"><button type="button" class="btn btn-light">Book Tickets</button></a>
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="image-wrapper">
+            <img src="images/candyman.jpg" alt="">
+            <div class="movie-info">
+              <p>
+                In present day, a decade after the last of the Cabrini towers were torn down, <br>
+                Anthony and his partner move into a loft in the now gentrified Cabrini. <br> 
+              </p>
+            </div>
+          </div>
+          <div class="button">
+            <p>
+              <a href="#"><button type="button" class="btn btn-light">Book Tickets</button></a>
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="image-wrapper">
+            <img src="images/tatb.jpg" alt="">
+            <div class="movie-info">
+              <p>
+                Senior year of high school takes center stage as Lara Jean returns from a family trip to Korea <br>
+                and considers her college plans -- with and without Peter.<br>
+               
+            </div>
+          </div>
+          <div class="button">
+            <p>
+              <a href="#"><button type="button" class="btn btn-light">Book Tickets</button></a>
+            </p>
+          </div>
+        </div>
+      </div>
+    
+    
+    </div>
+    
+
+    
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    
+  </body>
+
+  <script>
+    const imageItems=document.querySelectorAll('.image-wrapper')
+    imageItems.forEach(imageItem => {
+      imageItem.addEventListener('mouseover', () => {
+        imageItem.childNodes[1].classList.add('dark-image'); 
+      })
+
+      imageItem.addEventListener('mouseout', () => {
+        imageItem.childNodes[1].classList.remove('dark-image'); 
+      })
+      
+    });
+      
+  </script>
+</html>
+
+<?php include "footer.html" ?>
